bb2bdab878f3e84cca3dceb184f1b4b9f30192b6
lilblackboot <131806378+lilblackboot@users.noreply.github.cogit config --global user.email 131806378+lilblackboot@users.noreply.github.com>
2024-12-18 13:49:06 +0530

    dependencies installed

---
+++ pinteresty/app/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Image from 'next/image'
+import { HiSearch,HiChat,HiBell } from 'react-icons/hi'
+
+function Header() {
+  return (
+    <div className='flex gap-3 md:gap-2 items-center p-6'>
+        <Image src='/logo.png' alt='logo' width={50} height={50}
+        className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/>
+        <button className='bg-black text-white p-2 px-4 rounded-full'>home</button>
+        <button className='font-semibold p-2 px-4 rounded-full'>create</button>
+        <div className='w-full bg-[#e9e9e9] p-3 flex hidden md:flex gap-3 items-center rounded-full'>
+            <HiSearch className='text-[25px] text-gray-500'/>
+            <input type='text' placeholder='search'
+            className='bg-transparent outline-none' />
+        </div>
+        <HiSearch className='text-[25px] text-gray-500 md:hidden'/>
+        <HiBell className='text-[40px] text-gray-500'/>
+        <HiChat className='text-[40px] text-gray-500'/>
+        <Image src={'/pfp.jpg'} alt='pfp' width={50} height={50}
+        className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/> 
+
+    </div>
+  )
+}
+
+export default Header
