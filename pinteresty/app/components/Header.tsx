41452f73c67266199586f0bd447eb55aa7add392
lilblackboot <131806378+lilblackboot@users.noreply.github.cogit config --global user.email 131806378+lilblackboot@users.noreply.github.com>
2024-12-19 23:03:53 +0530

    minute changes

--- pinteresty/app/components/Header.tsx
+++ pinteresty/app/components/Header.tsx
@@ -46,7 +46,7 @@ const saveUserInfo=async ()=>{
         <HiSearch className='text-[25px] text-gray-500 md:hidden'/>
         <HiBell className='text-[40px] text-gray-500'/>
         <HiChat className='text-[40px] text-gray-500'/>
-        {session?.user?  <Image src={session?.user?.image} alt='pfp' width={50} height={50}
+        {session?.user?  <Image src={session?.user?.image||"./pfp.png"} alt='pfp' width={50} height={50}
         className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/> :
         <button onClick={() => signIn()} className='font-semibold p-2 px-4 rounded-full'>login</button>}
 
