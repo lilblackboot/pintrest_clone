500566b69a1d5f4854b11b43de4b6f21a3edb4b0
lilblackboot <131806378+lilblackboot@users.noreply.github.cogit config --global user.email 131806378+lilblackboot@users.noreply.github.com>
2024-12-19 21:53:28 +0530

    login page successfull done for fucks sake

--- pinteresty/app/components/Header.tsx
+++ pinteresty/app/components/Header.tsx
@@ -1,8 +1,12 @@
+"use client"
 import React from 'react'
 import Image from 'next/image'
 import { HiSearch,HiChat,HiBell } from 'react-icons/hi'
+import { signIn,useSession,signOut } from 'next-auth/react'
 
 function Header() {
+  const { data: session } = useSession()
+  console.log(session)
   return (
     <div className='flex gap-3 md:gap-2 items-center p-6'>
         <Image src='/logo.png' alt='logo' width={50} height={50}
@@ -17,8 +21,10 @@ function Header() {
         <HiSearch className='text-[25px] text-gray-500 md:hidden'/>
         <HiBell className='text-[40px] text-gray-500'/>
         <HiChat className='text-[40px] text-gray-500'/>
-        <Image src={'/pfp.jpg'} alt='pfp' width={50} height={50}
-        className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/> 
+        {session?.user?  <Image src={session?.user?.image} alt='pfp' width={50} height={50}
+        className='hover:bg-gray-300 p-2 rounded-full cursor-pointer'/> :
+        <button onClick={() => signIn()} className='font-semibold p-2 px-4 rounded-full'>login</button>}
+
 
     </div>
   )
