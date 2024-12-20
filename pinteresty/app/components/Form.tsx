808515846ac7ecb8db0509f9cfbddf6fd45a14c3
lilblackboot <131806378+lilblackboot@users.noreply.github.cogit config --global user.email 131806378+lilblackboot@users.noreply.github.com>
2024-12-20 16:21:25 +0530

    states for input in pin-builder done

--- pinteresty/app/components/Form.tsx
+++ pinteresty/app/components/Form.tsx
@@ -1,22 +1,34 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
 import UploadImage from './UploadImage'
 import UserTag from './UserTag'
+import { useSession } from 'next-auth/react'
 
 function Form() {
+    const {data:session}=useSession()
+    const [title,setTitle] = useState()
+    const [desc,setDesc] = useState()
+    const [link,setLink] =useState()
+    const [file,setFile] =useState()
+
+    const onSave=()=>{
+        console.log("title : ",title ,'discription: ',desc,'link: ',link, 'file: ',file)
+    }
+
   return (
     <div className='bg-white p-16 rounded-2xl'>
-        <div className='bg-red-500 p-2 text-white font-semibold px-3 rounded-lg'>
+        <div onClick={onSave} className='bg-red-500 p-2 text-white font-semibold px-3 rounded-lg'>
             Save
         </div>
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-10'>
-            <UploadImage/>
+            <UploadImage setFile={(file)=>setFile(file)}/>
             <div className='col-span-2'>
                 <div className='w-[100%]'>
-                    <input type='text' placeholder='Add your title' className='text-[35px] outline-none font-bold w-full border-b-[2px] border-gray-400 placeholder-gray-400'/>
+                    <input type='text' onChange={(e)=>{setTitle(e.target.value)}} placeholder='Add your title' className='text-[35px] outline-none font-bold w-full border-b-[2px] border-gray-400 placeholder-gray-400'/>
                     <h2 className='text-[12px] w-full text-gray-400'>the first 40 characters are what usually shows up in the feed</h2>
                     <UserTag />
-                    <textarea type='text' placeholder='Tell everyone what the pin is about' className='outline-none w-full mt-8 pb-4 text-[14px] border-b-[2px] border-gray-400 placeholder-gray-400'/>
-                    <input type='text' placeholder='Add a destination link' className='outline-none w-full pb-4 mt-[90px] border-b-[2px] border-gray-400 placeholder-gray-400'/>
+                    <textarea type='text' onChange={(e)=>{setDesc(e.target.value)}} placeholder='Tell everyone what the pin is about' className='outline-none w-full mt-8 pb-4 text-[14px] border-b-[2px] border-gray-400 placeholder-gray-400'/>
+                    <input type='text' onChange={(e)=>{setLink(e.target.value)}} placeholder='Add a destination link' className='outline-none w-full pb-4 mt-[90px] border-b-[2px] border-gray-400 placeholder-gray-400'/>
                 </div>
             </div>
         </div>
--- pinteresty/app/components/UploadImage.tsx
+++ pinteresty/app/components/UploadImage.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import { useSession } from 'next-auth/react'
+import React, { useState } from 'react'
 import { HiArrowUpCircle } from 'react-icons/hi2'
 
-function UploadImage() {
+function UploadImage({setFile}) {
+  
+
   return (
     <div>
         <label className='m-5 flex flex-col justify-center items-center cursor-pointer h-[90%] border-[2px] border-gray-300 border-dashed rounded-lg taxt-gray-600'>
             <HiArrowUpCircle className='text-[22px]'/>
             <h2 className='font-semibold'>Click to upload</h2>
-            <input id='dropzone-file' type='file' className='hidden'/>
+            <input id='dropzone-file' type='file' className='hidden' onChange={(e)=>{setFile(e.target.files[0])}}/>
         </label>
     </div>
   )
